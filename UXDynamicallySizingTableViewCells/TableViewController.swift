a27259e1567aa5b151d60f2ae470670ac549ed0b
Michael Nino Evensen <michaelevensen@gmail.com>
2016-10-27 15:21:20 +0200

    adjustments

--- UXDynamicallySizingTableViewCells/TableViewController.swift
+++ UXDynamicallySizingTableViewCells/TableViewController.swift
@@ -12,7 +12,7 @@ class TableViewController: UITableViewController {
 
     let data = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet odio consequat, mollis dui quis, pulvinar enim. Mauris mollis metus sit amet porta dapibus.",
-        "Aliquam aliquam rhoncus suscipit. Donec scelerisque aliquam dolor, a auctor velit lacinia non. Pellentesque eget sapien quam. Ut efficitur ligula eget erat cursus, sit amet dapibus turpis eleifend. Duis neque eros, convallis vitae velit ut, tempor suscipit lacus. Aliquam pharetra cursus tellus. ",
+        "Aliquam aliquam rhoncus suscipit. Donec scelerisque aliquam dolor, a auctor velit lacinia non. Pellentesque eget sapien quam. Ut efficitur ligula eget erat cursus, sit amet dapibus turpis eleifend. Duis neque eros, convallis vitae velit ut, tempor suscipit lacus. Aliquam pharetra cursus tellus.",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dictum urna ut pulvinar tristique. Proin sapien nisl, scelerisque vel eros sed, finibus sagittis mi. Curabitur rutrum turpis sit amet eleifend interdum. Phasellus nec leo eu nisl tempus luctus. Sed ultrices porta erat nec lacinia. Etiam diam est, venenatis vel sodales vel, aliquam sed metus. Donec sit amet finibus turpis. Aenean vel porttitor ante. Curabitur venenatis vulputate leo, varius placerat augue congue eget. Cras quis dui nunc. Praesent pharetra rhoncus urna, eget feugiat augue congue a.",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     ]
